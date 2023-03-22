358f8b6cca1c1aa6221570f4e8bfc6189d175074
hmzaib <118712017+hmzaib@users.noreply.github.com>
2023-03-22 13:10:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more inf
+using System;
+namespace Conditional
+{
+    class IfStatement
+    {
+        public static void Main(string[] args)
+        {
+            int value = 3;
+            if (value < 10)
+            {
+                Console.WriteLine("{0} is less than 10", value);
+            }
+            Console.WriteLine("this statement is always executed");
+
+        }
+    }
+}
